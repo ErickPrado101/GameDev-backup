0859e1b5b151dd855f3a4c214b0e64dd30db2a19
ErickPrado101 <epg1.prado@gmail.com>
2026-03-01 00:04:39 -0300

    personagem-anições,ajuste da vamera,ajustar posições das plantas

--- player.gd
+++ player.gd
@@ -5,41 +5,53 @@ const SPEED = 300.0
 var target_position: Vector2
 var moving_to_click := false
 
+@onready var anim = $AnimatedSprite2D
+
 func _ready():
 	target_position = global_position
 
-func _physics_process(delta: float) -> void:
-	
-	# =========================
-	# MOVIMENTO POR TECLADO
-	# =========================
+func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	
 	input_vector.x = Input.get_axis("ui_left", "ui_right")
 	input_vector.y = Input.get_axis("ui_up", "ui_down")
 	
-	# Se apertar teclado, cancela movimento por clique
 	if input_vector != Vector2.ZERO:
 		moving_to_click = false
-		input_vector = input_vector.normalized()
-		velocity = input_vector * SPEED
-	
-	# =========================
-	# MOVIMENTO POR MOUSE
-	# =========================
+		velocity = input_vector.normalized() * SPEED
 	elif moving_to_click:
-		var direction = (target_position - global_position)
-		
+		var direction = target_position - global_position
 		if direction.length() > 5:
 			velocity = direction.normalized() * SPEED
 		else:
 			moving_to_click = false
 			velocity = Vector2.ZERO
-	
 	else:
 		velocity = Vector2.ZERO
 	
 	move_and_slide()
+	update_animation()
+
+
+func update_animation():
+	if velocity.length() < 5:
+		anim.play("idle")
+		return
+	
+	if velocity.x != 0:
+		if velocity.x > 0:
+			anim.play("ui_right")
+			anim.flip_h = false
+		else:
+			anim.play("ui_right")
+			anim.flip_h = true
+	else:
+		if velocity.y < 0:
+			anim.play("ui_right")
+			anim.flip_h = false
+		else:
+			anim.play("ui_right")
+			anim.flip_h = true
 
 
 func _input(event):
